5bf6b711693e8c76447a5654737efe5220ec1a94
sg3163 <sg3163@columbia.edu>
2014-03-04 03:04:02 -0500

    Trying to resolve class not found error

--- TwittTube/src/main/java/edu/columbia/servlets/VideoUploaderServlet.java
+++ TwittTube/src/main/java/edu/columbia/servlets/VideoUploaderServlet.java
@@ -43,10 +43,6 @@ public class VideoUploaderServlet extends HttpServlet {
 		
 		S3BucketManager m = new S3BucketManager();
 		
-		/*
-		boolean isMultipart = ServletFileUpload.isMultipartContent(request);
-		*/
-		
 		MultipartRequest mp = new MultipartRequest(request, ".", 1024 * 1024 * 1024);
 		Enumeration files = mp.getFileNames();
 		while (files.hasMoreElements())
@@ -55,20 +51,6 @@ public class VideoUploaderServlet extends HttpServlet {
 			   String filename = mp.getFilesystemName(name);
 			   String type = mp.getContentType(name);
 			   File f = mp.getFile(name);
-			   
-			   /*
-			   out.println("name: " + name);
-			   out.println("filename: " + filename);
-			   out.println("type: " + type);
-			   if (f != null) {
-			     out.println("f.toString(): " + f.toString());
-			     out.println("f.getName(): " + f.getName());
-			     out.println("f.exists(): " + f.exists());
-			     out.println("f.length(): " + f.length());
-			     out.println("f.path(): " + f.getAbsolutePath());
-			     out.println();
-			   }
-			   */
 			   
 			   String videoId = "";
 			   String userId = mp.getParameter("userid");
@@ -85,61 +67,8 @@ public class VideoUploaderServlet extends HttpServlet {
 				dao.saveVideoMetadata(videoId, userId, videoLoc, videoReplyTo);
 			  
 			   f.delete();
-			   
 		}
 		
-		// request.getRequestDispatcher("/videos.jsp").forward(request, response);
-		
-		/*
-		ServletFileUpload upload = new ServletFileUpload(new DiskFileItemFactory());
-		
-		try
-		{
-			List fileItems = upload.parseRequest(request);
-			Iterator i = fileItems.iterator();
-			
-			while ( i.hasNext () ) 
-		      {
-		         FileItem fi = (FileItem)i.next();
-		         
-		         if ( !fi.isFormField() )	
-		         {
-		            String fieldName = fi.getFieldName();
-		            String fileName = fi.getName();
-		            String contentType = fi.getContentType();
-		            boolean isInMemory = fi.isInMemory();
-		            long sizeInBytes = fi.getSize();
-		            File file;
-		            
-		            // Write the file
-		            if( fileName.lastIndexOf("\\") >= 0 )
-		            {
-		               file = new File(fileName.substring( fileName.lastIndexOf("\\"))) ;
-		            }
-		            else
-		            {
-		               file = new File(fileName.substring(fileName.lastIndexOf("\\")+1)) ;
-		            }
-		            
-		            fi.write( file ) ;
-		         
-		            m.putObject("video file", file);
-		            file.delete();
-		            
-		            out.println("Uploaded Filename: " + file.getName() + "<br>");
-		         }
-		         
-		      }
-		}
-		catch (FileUploadException e)
-		{
-			out.println(e.getMessage() + "<br>");
-		}
-		catch (Exception e)
-		{
-			out.println("exception");
-		}
-		*/
 	}
 
 }
