ac447b4f1bdb1996f00d4a0821fab3274455d681
Jason Lee <jslee0813@hotmail.com>
2014-03-07 15:55:23 -0500

    Merge branch 'master' of https://github.com/sg3163/TwittTube
    Conflicts:
    	TwittTube/src/main/java/edu/columbia/servlets/VideoUploaderServlet.java

--- TwittTube/src/main/java/edu/columbia/vo/User.java
+++ TwittTube/src/main/java/edu/columbia/vo/User.java
@@ -2,6 +2,10 @@ package edu.columbia.vo;
 
 public class User {
 	
+	public User() {
+		
+	}
+	
 	public User(String userId, String firstName, String lastName) {
 		this.userId = userId;
 		this.firstName = firstName;
@@ -11,6 +15,9 @@ public class User {
 	private String userId;
 	private String firstName;
 	private String lastName;
+	private String email;
+	private String phoneNumber;
+	private String groupNumber;
 	
 	public String getUserId() {
 		return userId;
@@ -29,5 +36,23 @@ public class User {
 	}
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	public String getGroupNumber() {
+		return groupNumber;
+	}
+	public void setGroupNumber(String groupNumber) {
+		this.groupNumber = groupNumber;
 	}
 }
